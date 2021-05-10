654fe1056e6ce4841a5c4c359d26f46d8a9dac00
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-10 19:12:02 -0400

    Modified the phonebook to hold and accept more than one entry. Fixed a couple bugs.

--- phonebook_app.py
+++ phonebook_app.py
@@ -1,44 +1,57 @@
 #Create a dictionary that can store entries and output that information to the user. 
 
 # Empty dictionary to hold all entries:
-catalog = {}
+catalog = {
+    "bob": "123-455-9678",
+    "judie": "444-301-5497"
+    
+}
 program_stop = True
 
-
+#Note 5/10 This could just be changed to while program_stop, the == true is just an extra step.
 while program_stop == True:
     print("\n Electronic Phone Book \n")
     print("======================")
     print("\n 1. Look up an entry \n 2. Set an entry \n 3. Delete an entry \n 4. List all entries \n 5. Quit")
     user_input = int(input("What do you want to do? Pick a number from 1 -5: "))
+    
     #Look up an Entry:
     if (user_input == 1):
         name_selection = input("Who would you like to look up? ")
-        if (name_selection == catalog["user_name"]):
-            print(catalog["user_name"] + " " + catalog["user_number"])
+        lower_case_name = name_selection.lower()
+        if lower_case_name in catalog:
+            #.capitalize capitalizes the first letter
+            print(name_selection.capitalize() + "'s phone number is: " + catalog[lower_case_name]) 
         else:
             print("Sorry, we don't have someone with that name in our database.")
+            
     # Set an Entry:
     if (user_input == 2):
         user_name = input("Enter your name: ")
         user_number = input("Enter your phone number: ")
-        catalog["user_name"] = user_name
-        catalog["user_number"] = user_number
-        print("You have added an entry for %s with the phone number: %s." % (catalog["user_name"], catalog["user_number"]))
+        lower_case_name = user_name.lower()
+        catalog[lower_case_name] = user_number
+        print("You have added an entry for " + user_name + ".")
+        
     #Delete an Entry:
     if (user_input == 3):
         print(catalog)
         entry_to_delete = input("Whose entry would you like to delete? ")
-        if entry_to_delete == catalog["user_name"]:
-            del catalog["user_name"]
-            del catalog["user_number"]
-            print("The entry has been deleted.")
+        lower_entry_to_delete = entry_to_delete.lower()
+        if lower_entry_to_delete in catalog:
+            del catalog[lower_entry_to_delete]
+            print("The entry for " + lower_entry_to_delete + " has been deleted.")
         else:
             print("We don't have anyone with that name in our database.")
+            
     # List all Entries:
     if (user_input == 4):
-        print(catalog)
+        for contact in catalog:
+            print("Found an entry for " + contact.capitalize() + ": " + catalog[contact])
+        
     #Quit the Program:
     if (user_input == 5):
         print("See you next time!")
         program_stop = False
+
         
---
+++ phonebook_app2.py
@@ -0,0 +1,61 @@
+#Use functions to make the phonebook_app cleaner.
+catalog = {
+    "bob": "123-455-9678",
+    "judie": "444-301-5497"
+    
+}
+
+program_stop = True
+
+def option1():
+    name_selection = input("Who would you like to look up? ")
+    lower_case_name = name_selection.lower()
+    if lower_case_name in catalog:
+        print(name_selection.capitalize() + "'s phone number is: " + catalog[lower_case_name]) 
+    else:
+        print("Sorry, we don't have someone with that name in our database.")
+
+def option2():
+    user_name = input("Enter your name: ")
+    user_number = input("Enter your phone number: ")
+    lower_case_name = user_name.lower()
+    catalog[lower_case_name] = user_number
+    print("You have added an entry for " + user_name + ".")
+
+def option3():
+    print(catalog)
+    entry_to_delete = input("Whose entry would you like to delete? ")
+    lower_entry_to_delete = entry_to_delete.lower()
+    if lower_entry_to_delete in catalog:
+        del catalog[lower_entry_to_delete]
+        print("The entry for " + lower_entry_to_delete + " has been deleted.")
+    else:
+        print("We don't have anyone with that name in our database.")
+
+def option4():
+    for contact in catalog:
+        print("Found an entry for " + contact.capitalize() + ": " + catalog[contact])
+
+def option5():
+    print("See you next time!")
+
+while program_stop:
+    print("\n Electronic Phone Book \n")
+    print("======================")
+    print("\n 1. Look up an entry \n 2. Set an entry \n 3. Delete an entry \n 4. List all entries \n 5. Quit")
+    user_input = int(input("What do you want to do? Pick a number from 1 -5: "))
+    if user_input == 1:
+        option1()
+    elif user_input == 2:
+        option2()
+    elif user_input == 3:
+        option3()
+    elif user_input == 4:
+        option4()
+    elif user_input == 5:
+        option5()
+        program_stop = False
+    else:
+        print("Please select an option using a number 1 -5.")
+    
+    
